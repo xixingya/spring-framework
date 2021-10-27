c8ab268473b3287f747d84028618c1befbfc77ed
liuzhifei <2679431923@qq.com>
2021-10-27 19:27:26 +0800

    add my demo

---
+++ springdemo/src/main/java/tech/xixing/demo/resource/ResourceDemo.java
@@ -0,0 +1,13 @@
+package tech.xixing.demo.resource;
+
+/**
+ * @author liuzhifei
+ * @version 1.0
+ * @date 2021/10/27 7:26 PM
+ */
+public class ResourceDemo {
+
+	public static void main(String[] args) {
+
+	}
+}
