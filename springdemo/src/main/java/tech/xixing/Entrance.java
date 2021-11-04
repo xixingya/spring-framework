63eb208e799e953b820c8b500a10812419222932
liuzhifei <2679431923@qq.com>
2021-11-04 10:12:47 +0800

    add my demo

---
+++ springdemo/src/main/java/tech/xixing/Entrance.java
@@ -0,0 +1,13 @@
+package tech.xixing;
+
+/**
+ * @author liuzhifei
+ * @version 1.0
+ * @date 2021/11/3 7:46 PM
+ */
+public class Entrance {
+
+	public static void main(String[] args) {
+
+	}
+}
--- springdemo/src/main/java/tech/xixing/demo/resource/ResourceDemo.java
+++ springdemo/src/main/java/tech/xixing/demo/resource/ResourceDemo.java
@@ -1,5 +1,12 @@
 package tech.xixing.demo.resource;
 
+import org.springframework.core.io.FileSystemResource;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
 /**
  * @author liuzhifei
  * @version 1.0
@@ -7,7 +14,15 @@ package tech.xixing.demo.resource;
  */
 public class ResourceDemo {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws Exception{
+		final FileSystemResource fileSystemResource = new FileSystemResource("/Users/liuzhifei/workspace/sourcecode/my/spring-framework/springdemo/src/main/java/tech/xixing/demo/resource/test.txt");
+		final File file = fileSystemResource.getFile();
+		System.out.println(file.length());
+		final OutputStream outputStream = fileSystemResource.getOutputStream();
+		final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
+		writer.append("hello world!");
+		writer.flush();
+		outputStream.close();
+		writer.close();
 	}
 }
